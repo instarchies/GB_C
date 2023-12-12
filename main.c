066e8627d51029bfbc396a78aed07eea7b52765f
ilya <ilya91103@gmail.com>
2023-12-13 00:01:13 +0300

    DZ1

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, world\n");
+    printf("\n");
+
+    printf("Lets`s\n");
+    printf("    go\n");
+    printf("      to walk\n");
+    printf("\n");
+
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+    printf("\n");
+
+    
+    printf(" _   _\n");
+    printf("/ \\_/ \\ \n");
+    printf("\\_/ \\_/\n");
+    printf("/ \\_/ \\ \n");
+    printf("\\_/ \\_/\n");
+    return 0;
+    
+}
