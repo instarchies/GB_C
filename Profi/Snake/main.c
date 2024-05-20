e8d203f8de149b4675c89ad2aa1ac0773f3face1
archie <ilya91103@gmail.com>
2024-05-20 22:58:37 +0300

    Snake 3.0

--- Profi/Snake/main.c
+++ Profi/Snake/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <ncurses.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
@@ -7,7 +7,7 @@
 
 #define WIDTH 40
 #define HEIGHT 20
-#define INITIAL_DELAY 100000 
+#define INITIAL_DELAY 100000
 
 struct Snake {
     int x, y;
@@ -19,6 +19,24 @@ struct Snake {
 struct Apple {
     int x, y;
 };
+
+
+void setColor(int objectType){
+    attroff(COLOR_PAIR(1));
+    attroff(COLOR_PAIR(2));
+    attroff(COLOR_PAIR(3));
+    switch(objectType){
+        case 1: 
+            attron(COLOR_PAIR(1));
+            break;
+        case 2:
+            attron(COLOR_PAIR(2));
+            break;
+        case 3: 
+            attron(COLOR_PAIR(3));
+            break;
+    }
+}
 
 int kbhit(void) {
     struct termios oldt, newt;
@@ -50,43 +68,47 @@ void Setup(struct Snake *snake, struct Apple *apple) {
     snake->nTail = 0;
 }
 
-void Draw(struct Snake snake, struct Apple apple, int score) {
-    system("clear");
-    for (int i = 0; i < WIDTH+2; i++)
-        printf("#");
-    printf("\n");
+void Draw(struct Snake snake, struct Apple apple, int score, int snakeColor) {
+    clear();
+    for (int i = 0; i < WIDTH + 2; i++)
+        mvprintw(0, i, "#");
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             if (j == 0)
-                printf("#");
-            if (i == snake.y && j == snake.x)
-                printf("@");
-            else if (i == apple.y && j == apple.x)
-                printf("*");
-            else {
+                mvprintw(i + 1, j, "#");
+            if (i == snake.y && j == snake.x) {
+                setColor(snakeColor); // Use selected snake color
+                mvprintw(i + 1, j + 1, "@");
+                setColor(0); 
+            } else if (i == apple.y && j == apple.x) {
+                setColor(3); 
+                mvprintw(i + 1, j + 1, "*");
+                setColor(0); 
+            } else {
                 int print = 0;
                 for (int k = 0; k < snake.nTail; k++) {
                     if (snake.tailX[k] == j && snake.tailY[k] == i) {
-                        printf("*");
+                        setColor(snakeColor); // Use selected snake color
+                        mvprintw(i + 1, j + 1, "*");
+                        setColor(0); 
                         print = 1;
                     }
                 }
                 if (print == 0)
-                    printf(" ");
+                    mvprintw(i + 1, j + 1, " ");
             }
-                
             if (j == WIDTH - 1)
-                printf("#");
+                mvprintw(i + 1, j + 1, "#");
         }
-        printf("\n");
     }
 
-    for (int i = 0; i < WIDTH+2; i++)
-        printf("#");
-    printf("\n");
-    printf("Your score:%d\n", score);
-    printf("Press 'P' to pause.\n");
+    for (int i = 0; i < WIDTH + 2; i++)
+        mvprintw(HEIGHT + 1, i, "#");
+    mvprintw(HEIGHT + 2, 0, "Your score: %d", score);
+    mvprintw(HEIGHT + 3, 0, "Press 'P' to pause.");
+    refresh();
 }
+
 
 void Input(struct Snake *snake, int *paused) {
     if (kbhit()) {
@@ -160,7 +182,6 @@ void Logic(struct Snake *snake, struct Apple *apple, int *score, int *delay) {
     for (int i = 0; i < snake->nTail; i++)
         if (snake->tailX[i] == snake->x && snake->tailY[i] == snake->y)
             exit(0);
-            
 
     if (snake->x == apple->x && snake->y == apple->y) {
         snake->nTail++;
@@ -173,30 +194,127 @@ void Logic(struct Snake *snake, struct Apple *apple, int *score, int *delay) {
     }
 }
 
+void startMenu(int *snakeColor) {
+    const char *choices[] = {
+        "Start Game",
+        "Choose Snake Color",
+        "Exit"
+    };
+    const char *colorChoices[] = {
+        "Red",
+        "Blue",
+        "Green"
+    };
+    int n_choices = sizeof(choices) / sizeof(choices[0]);
+    int n_colorChoices = sizeof(colorChoices) / sizeof(colorChoices[0]);
+    int highlight = 0;
+    int colorHighlight = 0;
+    int choice = -1;
+    int c;
+
+    while (1) {
+        clear();
+        mvprintw(0, 0, "Welcome to Snake Game");
+        for (int i = 0; i < n_choices; i++) {
+            if (i == highlight)
+                attron(A_REVERSE);
+            mvprintw(i + 2, 2, "%s", choices[i]);  // Use format string
+            if (i == highlight)
+                attroff(A_REVERSE);
+        }
+        refresh();
+
+        c = getch();
+        switch (c) {
+            case KEY_UP:
+                highlight = (highlight == 0) ? n_choices - 1 : highlight - 1;
+                break;
+            case KEY_DOWN:
+                highlight = (highlight == n_choices - 1) ? 0 : highlight + 1;
+                break;
+            case 10:  // Enter key
+                choice = highlight;
+                break;
+            default:
+                break;
+        }
+
+        if (choice != -1) {
+            if (choice == 0) {
+                // Start Game
+                return;
+            } else if (choice == 1) {
+                // Choose Snake Color
+                while (1) {
+                    clear();
+                    mvprintw(0, 0, "Choose Snake Color");
+                    for (int i = 0; i < n_colorChoices; i++) {
+                        if (i == colorHighlight)
+                            attron(A_REVERSE);
+                        mvprintw(i + 2, 2, "%s", colorChoices[i]);  // Use format string
+                        if (i == colorHighlight)
+                            attroff(A_REVERSE);
+                    }
+                    refresh();
+
+                    c = getch();
+                    switch (c) {
+                        case KEY_UP:
+                            colorHighlight = (colorHighlight == 0) ? n_colorChoices - 1 : colorHighlight - 1;
+                            break;
+                        case KEY_DOWN:
+                            colorHighlight = (colorHighlight == n_colorChoices - 1) ? 0 : colorHighlight + 1;
+                            break;
+                        case 10:  // Enter key
+                            *snakeColor = colorHighlight + 1; // 1 for Red, 2 for Blue, 3 for Green
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (c == 10)  // If Enter key was pressed
+                        break;
+                }
+            } else if (choice == 2) {
+                // Exit
+                endwin();
+                exit(0);
+            }
+            choice = -1; // Reset choice after handling
+        }
+    }
+}
+
 int main() {
-    srand(time(NULL));
-    struct Snake snake;
+    struct Snake snake1;
     struct Apple apple;
     int score = 0;
-    int paused = 0;
     int delay = INITIAL_DELAY;
-    int printPausedMessage = 1;
+    int paused = 0;
+    int snakeColor = 1; // Default to Red
 
-    Setup(&snake, &apple);
+    initscr();
+    noecho();
+    curs_set(FALSE);
+    keypad(stdscr, TRUE); // Enable arrow keys
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK); // Color for Snake (Red)
+    init_pair(2, COLOR_BLUE, COLOR_BLACK); // Color for Snake (Blue)
+    init_pair(3, COLOR_GREEN, COLOR_BLACK); // Color for Apple
+
+    startMenu(&snakeColor); // Show start menu and set snake color
+
+    Setup(&snake1, &apple);
+
     while (1) {
+        Draw(snake1, apple, score, snakeColor); // Pass snakeColor to Draw function
+        Input(&snake1, &paused);
         if (!paused) {
-            printPausedMessage = 1;
-            Draw(snake, apple, score);
-            Input(&snake, &paused);
-            Logic(&snake, &apple, &score, &delay);
-        } else {
-            if (printPausedMessage) {
-                printf("Paused. Press 'P' to resume.\n");
-                printPausedMessage = 0;
-            }
-            Input(&snake, &paused);
+            Logic(&snake1, &apple, &score, &delay);
+            usleep(delay);
         }
-        usleep(delay); 
     }
+
+    endwin();
     return 0;
 }
